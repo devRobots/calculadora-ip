79a048bd073258381dd380a8f099ecf3a57089bc
Yesid Rosas Toro <ysrosast_1@uqvirtual.edu.co>
2020-05-16 01:14:11 -0500

    Clase CalculadoraIP
    Se agrego contenido a la clase CalculadoraIP:
    * Propiedades (direccionIP, mascara)
    * Constructor 
    * Metodo para convertir la mascara a formato IPv4

--- src/main/kotlin/co/edu/uniquindio/modelo/CalculadoraIP.kt
+++ src/main/kotlin/co/edu/uniquindio/modelo/CalculadoraIP.kt
@@ -1,3 +1,36 @@
 package co.edu.uniquindio.modelo
 
-class CalculadoraIP
+class CalculadoraIP(ipFormat : String) {
+    private val direccionIp : String
+    private val mascara : Int
+
+    init {
+        val ipSplited = ipFormat.split("/")
+        direccionIp = ipSplited[0]
+        mascara = ipSplited[1].toInt()
+    }
+
+    fun maskToIpv4() : String {
+        var maskIpV4 = ""
+        var bitsRestantes = mascara
+        var bin = 0b0
+
+        for (bit in 0..31) {
+            bin *= 0b10
+            if (bitsRestantes > 0) {
+                bin += 0b1
+                bitsRestantes--
+            }
+
+            if ((bit + 1) % 8 == 0) {
+                maskIpV4 += SistemasNumericos.binToDec(bin)
+                if (bit < 31) {
+                    maskIpV4 += "."
+                }
+                bin = 0b0
+            }
+        }
+
+        return maskIpV4
+    }
+}
